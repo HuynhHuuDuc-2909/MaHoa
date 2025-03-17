b367f352746e4bbf3138a8a9186be61cb3c81c18
HuynhHuuDuc-2909 <huynhhuuduc069@gmail.com>
2025-03-17 09:53:44 +0700

    Update railfence_cipher.py

--- ex01_Bai2/Cipher/Railfence/railfence_cipher.py
+++ ex01_Bai2/Cipher/Railfence/railfence_cipher.py
@@ -12,9 +12,10 @@ class RailFenceCipher:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
-                rail_index += direction
-            cipher_text = ''.join(''.join(rail)for rail in rails)
-            return cipher_text
+            
+            rail_index += direction
+        cipher_text = ''.join(''.join(rail) for rail in rails)
+        return cipher_text
     
     def rail_fence_decrypt(self, cipher_text, num_rails):
         rail_lengths = [0] * num_rails
@@ -26,21 +27,21 @@ class RailFenceCipher:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
-                rail_index += direction
+            rail_index += direction
         rails = []
         start = 0
         for length in rail_lengths:
-            rails.append(cipher_text[start:start + length])
+            rails.append(list(cipher_text[start:start + length]))
             start += length
         plain_text = ""
         rail_index = 0
         direction = 1
         for _ in range(len(cipher_text)):
-            plain_text += rails[rail_index][0]
-            rails[rail_index] = rails[rail_index][1:]
+            plain_text += rails[rail_index].pop(0)
             if rail_index == 0:
                 direction = 1
             elif rail_index == num_rails - 1:
                 direction = -1
             rail_index += direction
+        
         return plain_text
